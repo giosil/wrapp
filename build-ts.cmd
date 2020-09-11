da9637faba4a0d84f65f0dbf97477d498dd5c56a
Giorgio Silvestris <giorgio.silvestris@gmail.com>
2020-09-11 17:03:59 +0200

    update

--- build-ts.cmd
+++ build-ts.cmd
@@ -1,17 +1,16 @@
 @echo off
 
+cd .\src\main\webapp
+
 echo Clean js folder..
-del /Q .\js\*.*
+del /Q .\wux\js\*.*
 
 echo Compile WUX...
 call tsc --declaration --project ./ts/wux/tsconfig.json
-echo Compile hello...
-call tsc --noEmitHelpers --declaration --project ./ts/tsconfig.json
 
 rem Install first https://www.npmjs.com/package/minifier
 echo Minify...
-call minify ./js/wux.js
-call minify ./js/hello.js
+call minify ./wux/js/wux.js
 
 rem Install first https://www.npmjs.com/package/uglify-js
 rem uglifyjs -c -o ./wux/js/wux.min.js -m -- ./wux/js/wux.js
