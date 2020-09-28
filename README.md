2d43b974420848bd7a0e37aa9375498873e7c392
Giorgio Silvestris <giorgio.silvestris@gmail.com>
2020-09-28 10:30:15 +0200

    update

--- README.md
+++ README.md
@@ -41,7 +41,7 @@ In **Wrapp** each page is mapped by the *Page* object and can be called up from 
     "/hello/css/main.css"
   ],
   "scripts": [
-    "/hello/js/wux.min.js",
+    "/wrapp/wux/js/wux.min.js",
     "/hello/js/hello.min.js"
   ],
   "attributes": {
--- hello/build-ts.cmd
+++ hello/build-ts.cmd
@@ -5,14 +5,11 @@ cd .\src\main\webapp
 echo Clean js folder..
 del /Q .\js\*.*
 
-echo Compile WUX...
-call tsc --declaration --project ./ts/wux/tsconfig.json
 echo Compile hello...
 call tsc --noEmitHelpers --declaration --project ./ts/tsconfig.json
 
 rem Install first https://www.npmjs.com/package/minifier
 echo Minify...
-call minify ./js/wux.js
 call minify ./js/hello.js
 
 rem Install first https://www.npmjs.com/package/uglify-js
