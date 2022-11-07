c1631d0b784174a110f602e6d3467f42b31be1f2
giosil <giorgio.silvestris@gmail.com>
2022-11-07 09:25:38 +0100

    update

---
+++ build-ts.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd .\src\main\webapp
+
+echo "Clean dist folder.."
+rm -fr ./wux/js
+
+echo "Compile WUX..."
+tsc --declaration --project ./ts/wux/tsconfig.json
+
+echo "Minify..."
+
+# Install first https://www.npmjs.com/package/minifier
+minify ./wux/js/wux.js
+
+# Install first https://www.npmjs.com/package/uglify-js
+# Usage: uglifyjs input_file -c (compress) -o (output_file) output_file
+# uglifyjs ./wux/js/wux.js -c -o ./wux/js/wux.min.js
+
